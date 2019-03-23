ff67f74a18a67768896e54b6b47f8756f24bf7df
Michael Currin <michael.currin@gmail.com>
2019-03-23 20:04:56 +0200

    feature: Add CSV graphing to main.js

--- public/main.js
+++ public/main.js
@@ -1,10 +1,13 @@
-const w = 1000;
-const h = 600;
-const linkDistance = 100;
-const colors = d3.scale.category10();
+/**
+ * Main network graphing script.
+ */
+const w = 1280;
+const h = 720;
+const linkDistance = 10;
 
 
-function tick (edges, nodes, nodelabels, edgepaths) {
+/** Handle object movement. **/
+function tick(edges, nodes, nodelabels, edgepaths) {
     edges.attr({
         "x1": function (d) {
             return d.source.x;
@@ -43,6 +46,7 @@ function tick (edges, nodes, nodelabels, edgepaths) {
 }
 
 
+/** Render network graph in target SVG using specified dataset. **/
 function draw(dataset) {
     var svg = d3.select("body").append("svg");
     svg.attr({
@@ -77,9 +81,7 @@ function draw(dataset) {
         .append("circle")
         .style("fill", '#08c')
         .call(force.drag)
-        .attr({
-            "r": 8
-        });
+        .attr("r", 8);
 
     var nodelabels = svg.selectAll(".nodelabel")
         .data(dataset.nodes)
@@ -116,5 +118,45 @@ function draw(dataset) {
     force.on("tick", () => tick(edges, nodes, nodelabels, edgepaths));
 }
 
-var filePath = 'sample.json';
-d3.json(filePath, draw);
+
+/**
+ * Convert CSV data to a dataset containing nodes and edges.
+ *
+ * Read a CSV with source and destination columns and convert to dataset with keys as edges
+ * and nodes. A name may be repeated across the source or target columns but will appear
+ * as a unique name in the nodes data. The edges data contains references to IDs in the nodes array.
+ */
+function csv(filePath) {
+    return new Promise(function (resolve) {
+        var nodesByName = {};
+
+        d3.csv(filePath, function (error, links) {
+            if (error)
+                throw error;
+
+
+            links.forEach(function (link) {
+                link.source = nodeByName(link.source);
+                link.target = nodeByName(link.target);
+            });
+
+            function nodeByName(name) {
+                return nodesByName[name] || (nodesByName[name] = {
+                    name: name
+                });
+            }
+
+            var nodes = d3.values(nodesByName);
+
+            var dataset = {
+                nodes: nodes,
+                edges: links
+            }
+            resolve(dataset)
+        })
+    })
+}
+
+
+csv("/sample.csv")
+    .then(dataset => draw(dataset));
